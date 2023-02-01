56217f75eb261a6d2af6fdaadfc0f88f5c0ae087
AGDMExM <explorers2016@yandex.ru>
2023-02-01 11:23:17 +0300

    Added new stream parser -length-prefixed Protobuf- #28

--- src/DelimitedMessagesStreamParser.py
+++ src/DelimitedMessagesStreamParser.py
@@ -1,39 +1,30 @@
-class DelimitedMessagesStreamParser:
-    # Get first message from buffer if exist
-    def _parse_delimited(self, bytes_: bytes, start: int):
-        a = bytes_[start:4]
-        sizeMessage = int.from_bytes(bytes_[start:4], byteorder='little')
-        # message = bytes_[start + 4, sizeMessage]
-        return (sizeMessage, start + 4)
+import messages_pb2
 
+class DelimitedMessagesStreamParser:
     def parse(self, bytes_: bytes) -> list:
         offset: int = 0
         messages_ = list()
-        currentMessage: tuple = self._parse_delimited(bytes_, 0)
+        currentMessage = messages_pb2.WrapperMessage()
+        tupleMsg: tuple = self._DecodeVarint32(bytes_, 0)
 
-        while currentMessage is not None:
+        while tupleMsg is not None:
+            currentMessage.ParseFromString(bytes_[tupleMsg[1] + 4:(tupleMsg[0] + tupleMsg[1] + 4)])
             messages_.append(currentMessage)
-            offset += messages_[-1][0] + 4
-            currentMessage = self._parse_delimited(bytes_, offset)
+            offset += tupleMsg[0] + 4
+            tupleMsg = self._DecodeVarint32(bytes_, offset)
 
         return messages_
+    
+    def _DecodeVarint32(self, bytes_: str, start: int) -> tuple:
+        if len(bytes_) < 4:
+            return None
 
-    def _DecodeVarint32(self, bytes: str, start: int):
-        pass
+        sizeMsg = int.from_bytes(bytes_[start: start + 4], byteorder='little')
 
+        if sizeMsg == 0:
+            return None
 
-# (message_size, pos) = _DecodeVarint32(bytes_, start)
- 
-# current_message = bytes_[pos:(frame_size + pos)]
- 
-# bytes_ - входные данные, полученные из сокета(например, прочитанные функцией readAll() или еще какой-то)
- 
-# start - это указание на начало интересующего нас куска в bytes_(там лежат N сообщений, предваренных размером в Varint32)
- 
-# для первого сообщения start равен 0
- 
-# message_size - это размер сообщения
- 
-# pos - это начало сообщения
- 
-# current_message - это само сообщение
+        if sizeMsg > len(bytes_) - start:
+            return None
+
+        return sizeMsg, start
